5923f04aaed0b7319c9b09e9cd6d2638a86bba71
dropkickedchild <grey.sparks@outlook.com>
2023-12-18 12:24:16 -0600

    added a loop so every 5 minutes the backend is pinged to keep it running.

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 // Dependencies
 require("dotenv").config();
+const axios = require("axios");
 const app = require("./utils/app");
 const mongo = require("./utils/mongo");
 const { PORT } = require("./constants");
@@ -22,6 +23,19 @@ async function main() {
 
 	// Use external routes
 	app.use("/auth", authRoutes);
+
+	// If the port is 10000 (render sets during deployment)
+	// ping the server every 5 minutes to prevent it from sleeping
+	// Hack so we don't have to pay $7 a month for 100% uptime.
+	if (PORT === 10000) {
+		setInterval(async () => {
+			try {
+				await axios.get("https://cavalier-1hi6.onrender.com/");
+			} catch (error) {
+				console.log(error);
+			}
+		}, 5 * 60 * 1000); // 5 minutes in milliseconds
+	}
 
 	// Run Express app
 	app.listen(PORT, () => {
